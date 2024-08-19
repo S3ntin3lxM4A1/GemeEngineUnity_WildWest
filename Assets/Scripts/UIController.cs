275e093917a0ae43be47ee325b7bd1edd809de5a
S3ntin3lxM4A1 <152184752+S3ntin3lxM4A1@users.noreply.github.com>
2024-08-19 11:56:46 +0200

    UI Start

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -5,7 +5,17 @@ using TMPro;
 
 public class UIController : MonoBehaviour
 {
-    public TMP_Text myText;
+    [Header("- - - - - - Main Menu - - - - - -")]
+    [Header("- - - - - - Play Scene - - - - - -")]
+    [Space(5)]
+    [Header("- - Before Play - -")]
+    public GameObject background;
+    public TMP_Text numText;
+    public float currentTime;
+    public float setTime;
+    [Space(5)]
+    [Header("----")]
+    public TMP_Text mytext;
     // Start is called before the first frame update
     void Start()
     {

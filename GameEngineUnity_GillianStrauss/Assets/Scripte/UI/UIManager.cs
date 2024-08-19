8334512ed7f94fa77d117f94a3959aa591224e65
S3ntin3lxM4A1 <152184752+S3ntin3lxM4A1@users.noreply.github.com>
2024-08-19 11:37:06 +0200

    Text Mesh was ist los

--- GameEngineUnity_GillianStrauss/Assets/Scripte/UI/UIManager.cs
+++ GameEngineUnity_GillianStrauss/Assets/Scripte/UI/UIManager.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using TMPro.Examples;
 
 public class UIManager : MonoBehaviour
 {
+    [Header("- - - - - - Main Menu - - - - - -")]
+    [Header("- - - - - - Play Scene - - - - - -")]
+    [Space(5)]
+    [Header("- - Before Play - -")]
+    public GameObject numBackground;
     // Start is called before the first frame update
     void Start()
     {
